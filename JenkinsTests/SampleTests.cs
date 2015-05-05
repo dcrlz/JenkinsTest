679287e3263f0673799f5823f8789b00a6d2ca5d
Daniel <dansartori1@gmail.com>
2015-05-05 16:20:34 -0300

    adding test project

---
+++ JenkinsTests/SampleTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace JenkinsTests
+{
+    [TestFixture]
+    public class SampleTests
+    {
+        [Test]
+        public void SumTwoValues()
+        {
+            var sum = 2 + 2;
+
+            Assert.AreEqual(4, sum);
+        }
+    }
+}
